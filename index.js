b2e9b6de6b78c7597b4d0baa1277840331c8dcfc
Andrew LeRoux <andrewleroux2639@gmail.com>
2021-11-12 20:16:13 -0800

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards(array, eventName) {
+    const newArray = [...array];
+    for (let i = 0; i < array.length; i++) {
+        newArray[i] = `Thank you, ${array[i]}, for the wonderful ${eventName} gift!`;
+}
+return newArray;
+}
+
+function countDown (integer) {
+    while (integer >= 0) {
+    console.log(integer);
+    integer--;
+    }
+}
